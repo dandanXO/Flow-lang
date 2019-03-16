bb5f13c3f8ea7a5a84639d2f4a876bdbaa89f510
GarbageCollections <eddy199883@gmail.com>
2019-03-17 06:25:25 +0800

    Rewrote lexer

--- flowc-bootstrap/Lexer.py
+++ flowc-bootstrap/Lexer.py
@@ -1,76 +1,171 @@
-from Tokens import TokenList, TokenReserveds, TokenTypes
+from Tokens import TokenList, TokenReserveds, TokenOperators
 import ply.lex as plex
 import re
 
-# plex will use this
-tokens = TokenList
-reserved_words = TokenReserveds + TokenTypes
+class Lexer:
+    # plex will use this
+    tokens = TokenList
+    
+    def __init__(self, text):
+        self.text = text
+        # Regex
+        self.hex_checker = re.compile(r'0x[^\s]([0-9a-f]*|[0-9A-F]*)')
+        self.binary_checker = re.compile(r'([10]+)b')
+    
+    def __iter__(self):
+        return self.lex.__iter__()
+    
+    def __next__(self):
+        return next(self.lex)
 
-# Regex
-hex_checker = re.compile(r'0x[^\s]([0-9a-f]*|[0-9A-F]*)')
-binary_checker = re.compile(r'([10]+)b')
+    def build(self, **args):
+        self.lex = plex.lex(object=self, **args)
+        self.lex.input(self.text)
+        self.lex.lineno = 1
 
+############################################
+#           LEXER DEFINATIONS        
+############################################
+    #Ignore space and tabs
+    t_ignore = ' \t' 
+        
+    # Capture string
+    t_STR_LITERAL= r'\"([^\\\n]|(\\.))*?\"'
 
-# Ignore comment
-t_ignore_COMMENT = r'#.*'
+    # Capture character
+    t_CHAR_LITERAL = r'\'\w{0,1}\''
+    
+    #register symbols
+    t_SYM_NOT = r'\!'
+    t_SYM_NOT_EQUAL = r'\!\='
+    t_SYM_AT = r'\@'
+    t_SYM_COLON = r'\:'
+    t_SYM_DOLLAR = r'\$'
+    t_SYM_REMAINDER = r'\%'
+    t_SYM_ASSIGN_REMAINDER = r'\%\='
+    t_SYM_XOR = r'\^'
+    t_SYM_ASSIGN_XOR = r'\^\='
+    t_SYM_AND = r'\&'
+    t_SYM_ASSIGN_AND = r'\&\='
+    t_SYM_ASTERISK = r'\*'
+    t_SYM_ASSIGN_MULTIPLY = r'\*\='
+    t_SYM_PAREN_OPEN = r'\('
+    t_SYM_PAREN_CLOSE = r'\)'
+    t_SYM_MINUS = r'\-'
+    t_SYM_MINUS_ONE = r'\-\-'
+    t_SYM_ASSIGN_SUBTRACT = r'\-\='
+    t_SYM_ADD = r'\+'
+    t_SYM_PLUS_ONE = r'\+\+'
+    t_SYM_ASSIGN_PLUS = r'\+\='
+    t_SYM_ASSIGN = r'\='
+    t_SYM_EQUAL = r'\=\='
+    t_SYM_BRACE_OPEN = r'\['
+    t_SYM_BRACE_CLOSE = r'\]'
+    t_SYM_BRACKET_OPEN = r'\{'
+    t_SYM_BRACKET_CLOSE = r'\}'
+    t_SYM_OR = r'\|'
+    t_SYM_ASSIGN_OR = r'\|\='
+    t_SYM_DIVIDE = r'\/'
+    t_SYM_ASSIGN_DIVIDE = r'\/\='
+    t_SYM_SEMICOLON = r'\;'
+    t_SYM_LESS = r'\<'
+    t_SYM_LESS_EQUAL = r'\<\='
+    t_SYM_SHIFT_LEFT = r'\<\<'
+    t_SYM_ASSIGN_SHIFT_LEFT = r'\<\<\='
+    t_SYM_GREATER = r'\>'
+    t_SYM_GREATER_EQUAL = r'\>\='
+    t_SYM_SHIFT_RIGHT = r'\>\>'
+    t_SYM_ASSIGN_SHIFT_RIGHT = r'\>\>\='
+    t_SYM_COMMA = r'\,'
+    t_SYM_RANGE = r'\~'
+    
+    # treat comment as newline
+    def t_ignore_COMMENT(self, t):
+        r'\#.*'
 
-# String
-t_STR_LITERAL= r'\"([^\\\n]|(\\.))*?\"'
+    #counting newline
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
 
-# Identifier
-def t_IDENTIFIER(t):
-    r"[_a-zA-Z][\w]*"
-    #change type if it is a reserved word
-    t.type = reserved_words.get(t.type, d="IDENTIFIER")
-    return t
-
-# Float literal
-def t_FLOAT_LITERAL(t):
-    r'(?:\d+)?\.(\d+)'
-    #convert string into float number
-    try:
-        t.value = float(t.value)
+    # Identifier
+    def t_IDENTIFIER(self, t):
+        r"[_a-zA-Z][\w]*"
+        #change type if it is a reserved word
+        t.type = TokenReserveds.get(t.value, "IDENTIFIER")
         return t
-    except ValueError:
-        pass
 
-#Binary numbers, heximal numbers and decimal numbers
-def t_INT_LITERAL(t):
-    r'(([10]+)b)|(^0x([0-9a-f]+|[0-9A-F]+))|([0-9]+)'
-
-    if binary_checker.match(t.value):
-        #it is a binary number
+    # Float literal
+    def t_FLOAT_LITERAL(self, t):
+        r'(?:\d+)?\.(\d+)'
+        #convert string into float number
         try:
-            #chop out the 'b' and attempt to convert it into a decimal number
-            t.value = int(t.value[:-1], 2) 
+            t.value = float(t.value)
             return t
         except ValueError:
             pass
-    elif hex_checker.match(t.value):
-        #heximal number
-        try:
-            t.value = int(t.value, 16)
-            return t
-        except ValueError:
-            pass
-    else:
-        #decimal number
-        # we won't accept numbers that start with zero
-        if len(t.value) > 1 and t.value[0] == '0':
-            pass
-        else:
+
+    #Binary numbers, heximal numbers and decimal numbers
+    def t_INT_LITERAL(self, t):
+        r'(([10]+)b)|(^0x([0-9a-f]+|[0-9A-F]+))|([0-9]+)'
+
+        if self.binary_checker.match(t.value):
+            #it is a binary number
             try:
-                t.value = int(t.value)
+                #chop out the 'b' and attempt to convert it into a decimal number
+                t.value = int(t.value[:-1], 2) 
                 return t
             except ValueError:
                 pass
-                
-def t_error(t):
-    print('Unknown token found!\n Text: {}'.format(t.value))
-    exit(-1)
+        elif self.hex_checker.match(t.value):
+            #heximal number
+            try:
+                t.value = int(t.value, 16)
+                return t
+            except ValueError:
+                pass
+        else:
+            #decimal number
+            # we won't accept numbers that start with zero
+            if len(t.value) > 1 and t.value[0] == '0':
+                pass
+            else:
+                try:
+                    t.value = int(t.value)
+                    return t
+                except ValueError:
+                    pass
+    
+    def t_error(self, t):
+        illegal_checker = re.compile(r'\'\w{2,}\'')
+        single_quote_checker = re.compile(r'\'.*$[^\']')
+        error_msg = 'Undefined token found!'
+        if illegal_checker.match(t.value):
+            error_msg = 'Character literal should not contain more than one characters!'
+        elif single_quote_checker.match(t.value):
+            error_msg = 'Quote did not paired!'
+        
+        #print out error message
+        print('\nError: {}\n\n```\n\n{}\n\n```\nin line {} at position {}'.
+            format(error_msg, t.value[:t.value.find('\n')], t.lineno, self.cal_column(t)))
+        exit(-1)
+
+    def cal_column(self, tok):
+        line_start = self.text.rfind('\n', 0, tok.lexpos) + 1
+        return (tok.lexpos - line_start) + 0
+
+    def print_token(self, t):
+        print('Token {} ({}) in line {} at pos {}'.format(t.type, t.value, t.lineno, self.cal_column(t)))
 
 # Unit test
 if __name__ == '__main__':
     print('Unit Test: Lexer')
+    with open('testcode/lexer.text.flo', 'r') as f:
+        test_code = f.read()
+
     #create lexer
-    lex = plex.lex()
+    lexer = Lexer(test_code)
+    lexer.build()
+
+    for tok in lexer:
+        lexer.print_token(tok)
--- flowc-bootstrap/Tokens.py
+++ flowc-bootstrap/Tokens.py
@@ -1,73 +1,70 @@
 
 # Reserved word
 TokenReserveds = {
-    'nil': 'kw_nil', 
-    'for': 'kw_for', 
-    'in': 'kw_in', 
-    'loop': 'kw_loop',
-    'continue': 'kw_continue', 
-    'break': 'kw_break', 
-    'if': 'kw_if', 
-    'else': 'kw_else',
-    'not': 'kw_not',
-    'or': 'kw_or', 
-    'xor': 'kw_xor', 
-    'and': 'kw_and',
-    'true': 'kw_true', 
-    'false': 'kw_false', 
-    'while': 'kw_while', 
-    'return': 'kw_return',
-    'match': 'kw_match', 
-    'const': 'kw_const', 
-    'struct': 'kw_struct', 
-    'func': 'kw_func', 
-    'stack': 'kw_stack', 
-    'let': 'kw_let'
-    }
-
-# Primitive types
-TokenTypes = {
-    'ptr': 'pt_ptr', 
-    'bool': 'pt_bool', 
-    'u8': 'pt_u8', 
-    'u16': 'pt_u16', 
-    'u32': 'pt_u32', 
-    'u64': 'pt_u64', 
-    'i8': 'pt_i8', 
-    'i16': 'pt_i16', 
-    'i32': 'pt_i32', 
-    'i64': 'pt_i64', 
-    'f32': 'pt_f32', 
-    'f64': 'pt_f64'
+    'nil': 'KW_NIL', 
+    'for': 'KW_FOR', 
+    'in': 'KW_IN', 
+    'loop': 'KW_LOOP',
+    'continue': 'KW_CONTINUE', 
+    'break': 'KW_BREAK', 
+    'if': 'KW_IF', 
+    'else': 'KW_ELSE',
+    'not': 'KW_NOT',
+    'or': 'KW_OR', 
+    'xor': 'KW_XOR', 
+    'and': 'KW_AND',
+    'true': 'KW_TRUE', 
+    'false': 'KW_FALSE', 
+    'while': 'KW_WHILE', 
+    'return': 'KW_RETURN',
+    'match': 'KW_MATCH', 
+    'const': 'KW_CONST', 
+    'struct': 'KW_STRUCT', 
+    'func': 'KW_FUNC', 
+    #'stack': 'KW_STACK', 
+    'let': 'KW_LET',
+    #Types
+    'ptr': 'PT_PTR', 
+    'bool': 'PT_BOOL', 
+    'u8': 'PT_U8', 
+    'u16': 'PT_U16', 
+    'u32': 'PT_U32', 
+    'u64': 'PT_U64', 
+    'i8': 'PT_I8', 
+    'i16': 'PT_I16', 
+    'i32': 'PT_I32', 
+    'i64': 'PT_I64', 
+    'f32': 'PT_F32', 
+    'f64': 'PT_F64'
     }
 
 # Operators
-TokenOperators = (\
-    # !, !=, @
-    'sym_not', 'sym_not_equal', 'sym_at', \
+TokenOperators = {\
+    # !, !=, @, :
+    '!': 'SYM_NOT', '!=': 'SYM_NOT_EQUAL', '@': 'SYM_AT', ':': 'SYM_COLON',\
     # $, %, %=, ^
-    'sym_dollar', 'sym_remainder', 'sym_assign_remain', 'sym_xor', \
+    '$': 'SYM_DOLLAR', '%': 'SYM_REMAINDER', '%=': 'SYM_ASSIGN_REMAINDER', '^': 'SYM_XOR', \
     # ^=, &, &=, *
-    'sym_assign_xor', 'sym_and', 'sym_assign_and', 'sym_asterisk', \
+    '^=': 'SYM_ASSIGN_XOR', '&': 'SYM_AND', '&=': 'SYM_ASSIGN_AND', '*': 'SYM_ASTERISK', \
     # *=, (, ), -
-    'sym_assign_multiply', 'sym_paren_open', 'sym_paren_close', 'sym_minus', \
+    '*=': 'SYM_ASSIGN_MULTIPLY', '(': 'SYM_PAREN_OPEN', ')': 'SYM_PAREN_CLOSE', '-': 'SYM_MINUS', \
     # --, -=, +, ++
-    'sym_minus_one', 'sym_assign_subtract', 'sym_add', 'sym_plus_one', \
+    '--': 'SYM_MINUS_ONE', '-=': 'SYM_ASSIGN_SUBTRACT', '+': 'SYM_ADD', '++': 'SYM_PLUS_ONE', \
     # +=, =, ==, [
-    'sym_assign_add', 'sym_assign', 'sym_equal', 'sym_brace_open', \
+    '+=': 'SYM_ASSIGN_PLUS', '=': 'SYM_ASSIGN', '==': 'SYM_EQUAL', '[': 'SYM_BRACE_OPEN', \
     # ], {, }, |
-    'sym_brace_close', 'sym_bracket_open', 'sym_bracket_close', 'sym_or', \
+    ']': 'SYM_BRACE_CLOSE', '{': 'SYM_BRACKET_OPEN', '}': 'SYM_BRACKET_CLOSE', '|': 'SYM_OR', \
     # |=, /, /=, ;
-    'sym_assign_or', 'sym_divide', 'sym_assign_divide', 'sym_semicolon', \
+    '|=': 'SYM_ASSIGN_OR', '/': 'SYM_DIVIDE', '/=': 'SYM_ASSIGN_DIVIDE', ';': 'SYM_SEMICOLON', \
     # <, <=, <<, <<=
-    'sym_less', 'sym_less_equal', 'sym_shift_left', 'sym_assign_shift_left', \
+    '<': 'SYM_LESS', '<=': 'SYM_LESS_EQUAL', '<<': 'SYM_SHIFT_LEFT', '<<=': 'SYM_ASSIGN_SHIFT_LEFT', \
     # >, >=, >>, >>=
-    'sym_greater', 'sym_greater_equal', 'sym_shift_right', 'sym_assign_shift_right', \
+    '>': 'SYM_GREATER', '>=': 'SYM_GREATER_EQUAL', '>>': 'SYM_SHIFT_RIGHT', '>>=': 'SYM_ASSIGN_SHIFT_RIGHT', \
     # , ~
-    'sym_comma', 'sym_range') 
+    ',': 'SYM_COMMA', '~': 'SYM_RANGE'}
 
 # Shapeless tokens
-TokenShapeless = ('IDENTIFIER', 'STR_LITERAL', 'FLOAT_LITERAL', 'INT_LITERAL')
+TokenShapeless = ['IDENTIFIER', 'STR_LITERAL', 'CHAR_LITERAL', 'FLOAT_LITERAL', 'INT_LITERAL']
 
 # Contain all tokens
-TokenList = list(TokenReserveds) + list(TokenTypes) + TokenOperators + TokenShapeless
+TokenList = list(TokenReserveds.values()) + list(TokenOperators.values()) + TokenShapeless
